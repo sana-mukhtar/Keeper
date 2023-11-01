cd8bbb60ad1519b5d954ffe6486ffca00926b6a8
Sana Mukhtar <130250734+sana-mukhtar@users.noreply.github.com>
2023-11-02 03:31:45 +0530

    modified contentEditable and spellCheck

--- src/Note.jsx
+++ src/Note.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Note(){
     return (
         <div className='Note'>
-        <h4>Note Title</h4>
-        <p> This is a Note Content</p>
+        <h4 contentEditable="true" spellCheck="true">Note Title</h4>
+        <p contentEditable="true" spellCheck="true"> This is a Note Content</p>
         </div>
        )
 }
